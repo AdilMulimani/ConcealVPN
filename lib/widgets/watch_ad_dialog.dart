98e80ec6469c6dcf1a6a7c152f45eb72fb30615a
AdilMulimani <adilmulimani@gmail.com>
2025-03-08 19:50:56 +0530

    feat(widgets): add watch_ad_dialog.dart

---
+++ lib/widgets/watch_ad_dialog.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class WatchAdDialog extends StatelessWidget {
+  final VoidCallback onComplete;
+
+  const WatchAdDialog({super.key, required this.onComplete});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text('Change Theme'),
+      content: Text('Watch an Ad to Change App Theme.'),
+      actions: [
+        CupertinoDialogAction(
+            isDefaultAction: true,
+            textStyle: TextStyle(
+              color: Colors.amberAccent,
+            ),
+            child: Text('Watch Ad'),
+            onPressed: () {
+              Get.back();
+              onComplete();
+            }),
+      ],
+    );
+  }
+}
